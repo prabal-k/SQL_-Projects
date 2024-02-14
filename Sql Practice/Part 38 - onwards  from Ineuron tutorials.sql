c0bd431e640457221414d47d9f112ff760b67577
prabal kuinkel <prabalkuinkel10@gmail.com>
2024-02-14 18:35:38 +0700

    Numeric functions in sql

--- Sql Practice/Part 38 - onwards  from Ineuron tutorials.sql
+++ Sql Practice/Part 38 - onwards  from Ineuron tutorials.sql
@@ -1,5 +1,7 @@
 use sales_analysis;
 -- Ineuron intelligence channel
+-- String functions are : length,replace,substr,lower,upper,mid,concat,instr,left,right,trim,
+
 -- +++++++++++++++Part 38) Substring () function 
 -- -- Extracts a portion of a string from text/column
 -- -- syntax:
@@ -9,3 +11,30 @@ Select dob,substr(dob,7,4) as dob_year
 from customer_dim;
 
 Select email,substr(email,1,length(email)) as str_email from customer_dim;
+
+-- +++++++++++++++Part 39) trim () function
+--  Trims or removes the white spaces from start and end of the column / string
+-- syntax: 
+-- trim(column_name)
+
+Select '           Name   'as nam,length('           Name   ') as name_length ,trim('           Name   ') as trimmed_name;
+
+
+-- Numeric functions starts from here
+-- performs operation on numeric columns/numeric data
+
+-- +++++++++++++++Part 40) abs () function
+-- converts -ve value into +value or give absolute value of the column
+-- for eg: if a price column consist of -ve value
+-- Syntax:
+-- abs(column_name)
+
+-- +++++++++++++++Part 41) mod () function
+-- Returns the remainder of particular division
+-- Syntax:
+-- mod(dividend,divisor) 
+-- for eg:
+-- mod(19,4) then output will be 3
+
+
+
